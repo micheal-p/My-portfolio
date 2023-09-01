4d28d69d4194bd782cb07de6773814e52b0380a0
micheal-p <58996608+micheal-p@users.noreply.github.com>
2023-09-01 12:47:21 +0100

    Add files via upload

---
+++ my porfolio/js/scripts.js
@@ -0,0 +1,5 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
